5548a66a965213282f6c2fbbfbca8fd9692421df
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-22 20:54:41 +0900

    fix: add PartialEq

--- src/calclator/ast/expression.rs
+++ src/calclator/ast/expression.rs
@@ -1,10 +1,12 @@
-use constant::Constant;
-use plus::Plus;
+use super::constant::Constant;
+// use super::plus::Plus;
+use super::operation::BinaryOperation;
 
 /// Expression
+#[derive(Debug, PartialEq)]
 pub enum Expression {
     Constant(Constant),
-    Plus(Box<Plus>),
+    BinaryOperation(Box<BinaryOperation>),
 }
 
 impl Expression {
@@ -12,7 +14,7 @@ impl Expression {
     pub fn eval(&self) -> i32 {
         match self {
             Expression::Constant(e) => e.eval(),
-            Expression::Plus(e) => e.eval(),
+            Expression::BinaryOperation(e) => e.eval(),
         }
     }
 }
