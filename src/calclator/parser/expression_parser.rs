3c0a3be5611172c93bc8f5252747507558746287
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-22 22:28:01 +0900

    fix: add paren

--- src/calclator/parser/expression_parser.rs
+++ src/calclator/parser/expression_parser.rs
@@ -1,4 +1,5 @@
 use nom::IResult;
+use nom::character::complete::char;
 use crate::calclator::ast::expression::Expression;
 use super::constant_parser::constant_parser;
 
@@ -6,4 +7,13 @@ use super::constant_parser::constant_parser;
 pub fn expression_parser(sent: &str) -> IResult<&str, Expression> {
     constant_parser(sent)
         .map(|(no_used, parsed)| (no_used, Expression::Constant(parsed)))
+}
+
+/// parser for Expression with parenthses
+pub fn expression_parser_with_paren(sent: &str) -> IResult<&str, Expression> {
+    let (no_used, _) = char('(')(sent)?;
+    let (no_used, expression) = expression_parser(no_used)?;
+    let (no_used, _) = char(')')(no_used)?;
+
+    Ok((no_used, expression))
 }
