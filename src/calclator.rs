bf86c957e1eedc2ed9fa75dd9483fce16a6a6a94
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-23 14:29:56 +0900

    fix: add evaluation

--- src/calclator.rs
+++ src/calclator.rs
@@ -1,2 +1,20 @@
 pub mod ast;
 pub mod parser;
+
+use nom::Err;
+use nom::error::ErrorKind;
+use parser::expression_parser::expression_parser;
+
+/// evaluate parsed expression
+pub fn expression_evaluate(sent: &str) -> Result<i32, Err<(&str, ErrorKind)>> {
+    expression_parser(sent)
+        .map(|(_, expression)| expression.eval())
+}
+
+#[test]
+fn test_expression_evaluate() {
+    assert_eq!(
+        expression_evaluate("1+2+3+4+5").unwrap(),
+        1 + 2 + 3 + 4 + 5
+    );
+}
