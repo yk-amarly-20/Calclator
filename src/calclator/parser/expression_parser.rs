d152411738c2c8d3e1b6598e3642393f04ef0c04
Rist063 <rist063@Rist063noMacBook-Air.local>
2020-09-23 14:02:12 +0900

    fix: implement add and minus

--- src/calclator/parser/expression_parser.rs
+++ src/calclator/parser/expression_parser.rs
@@ -1,12 +1,51 @@
 use nom::IResult;
 use nom::character::complete::char;
+use nom::combinator::{map, opt};
+use nom::branch::alt;
+use nom::sequence::tuple;
 use crate::calclator::ast::expression::Expression;
-use super::constant_parser::constant_parser;
+use crate::calclator::ast::operation::{BinaryOperation, Operation};
+// use super::constant_parser::constant_parser;
+use super::term_parser::term_parser;
+// use super::factor_parser::factor_parser;
 
 /// parser for Expression
 pub fn expression_parser(sent: &str) -> IResult<&str, Expression> {
-    constant_parser(sent)
-        .map(|(no_used, parsed)| (no_used, Expression::Constant(parsed)))
+    let operational_parser = map(
+        alt((char('+'), char('-'))),
+        |option|
+            match option {
+                '+' => Operation::_Plus,
+                '-' => Operation::_Minus,
+                _ => panic!("No Operation!!"),
+            }
+    );
+
+    let binary_operation_parser = tuple((
+        term_parser,
+        opt(
+            tuple((
+                operational_parser,
+                expression_parser
+            ))
+        )
+    ));
+
+    map(binary_operation_parser, |(head_expression, tail_operation)| {
+        if let Option::Some((operation, tail_expression)) = tail_operation {
+            Expression::BinaryOperation(
+                Box::new(
+                    BinaryOperation::new(
+                        operation,
+                        head_expression,
+                        tail_expression
+                    )
+                )
+            )
+        } else {
+            head_expression
+        }
+    })(sent)
 }
 
 /// parser for Expression with parenthses
